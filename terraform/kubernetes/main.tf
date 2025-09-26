54abf7cd2af3bbe8f5968ad423dca05f2ce423de
Ivana Koceva <ivana.koceva10@gmail.com>
2025-09-27 01:22:39 +0200

    changed kubernetes config

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -121,3 +121,146 @@ resource "kubernetes_deployment" "blog-deployment" {
     }
   }
 }
+
+
+resource "kubernetes_stateful_set" "blogdb" {
+  metadata {
+    name      = "blogdb"
+    namespace = kubernetes_namespace.blogs.metadata.0.name
+  }
+
+  spec {
+    service_name = "blogdb"
+    replicas     = 1
+
+    selector {
+      match_labels = {
+        app = "blogdb"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "blogdb"
+        }
+      }
+
+      spec {
+        container {
+          name  = "postgres"
+          image = "postgres:latest"
+
+          port {
+            container_port = 5432
+          }
+
+          env {
+            name = "POSTGRES_USER"
+            value_from {
+              secret_key_ref {
+                name = "blog-secrets"
+                key  = "POSTGRES_USER"
+              }
+            }
+          }
+
+          env {
+            name = "POSTGRES_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = "blog-secrets"
+                key  = "POSTGRES_PASSWORD"
+              }
+            }
+          }
+
+          env {
+            name = "POSTGRES_DB"
+            value_from {
+              config_map_key_ref {
+                name = "blog-config"
+                key  = "POSTGRES_DB"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+# create services for app and db
+resource "kubernetes_service" "blog-service" {
+    metadata {
+        name = "blog-service"
+        namespace = kubernetes_namespace.blogs.metadata.0.name
+    }
+
+    spec {
+        selector = { 
+            app = "blog"
+        }
+
+        port {
+            protocol = "TCP"
+            port = 80
+            target_port = 8080
+        }
+        type = "ClusterIP"
+    }
+}
+
+resource "kubernetes_service" "blogdb" {
+    metadata {
+        name = "blogdb"
+        namespace = kubernetes_namespace.blogs.metadata.0.name
+    }
+
+    spec {
+        cluster_ip = "None"
+        selector = { 
+            app = "blogdb"
+        }
+
+        port {
+            protocol = "TCP"
+            port = 5432
+            target_port = 5432
+        }
+    }
+}
+
+# create ingress
+resource "kubernetes_ingress_v1" "blog-ingress" {
+    metadata {
+        name = "blog-ingress"
+        namespace = kubernetes_namespace.blogs.metadata.0.name
+    }
+
+    spec {
+    ingress_class_name = "nginx"
+
+    rule {
+      host = "blog.local"
+
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "blog-service"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
