709881a9b215665d48905cbfecab5daf660163b1
Ivana Koceva <ivana.koceva10@gmail.com>
2025-10-25 19:41:02 +0200

    changed postgres image version

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -4,7 +4,7 @@ resource "docker_image" "blog_app" {
 }
 
 resource "docker_image" "blog_db" {
-  name = "postgres:latest"
+  name = "postgres:17-alpine"
 }
 
 # create network
@@ -12,29 +12,34 @@ resource "docker_network" "blog_network" {
   name = "blog_network"
 }
 
+# create volume
+resource "docker_volume" "blogdb_data" { 
+  name = "blogdb_data"
+}
+
 # create containers
 resource "docker_container" "db_service" {
   image = docker_image.blog_db.image_id
-  name = "db_service"
-  
+  name  = "db_service"
+
   networks_advanced {
-     name = docker_network.blog_network.name
+    name = docker_network.blog_network.name
   }
-  
+
   ports {
-     internal = 5432
-     external = var.postgres_port
+    internal = 5432
+    external = var.postgres_port
   }
-  
+
   env = [
-     "POSTGRES_DB=${var.postgres_db}",
-     "POSTGRES_USER=${var.postgres_user}",
-     "POSTGRES_PASSWORD=${var.postgres_password}"
+    "POSTGRES_DB=${var.postgres_db}",
+    "POSTGRES_USER=${var.postgres_user}",
+    "POSTGRES_PASSWORD=${var.postgres_password}"
   ]
 
   # created local volume for container to mount
   volumes {
-    volume_name    = "blogdb_data"
+    volume_name    = docker_volume.blogdb_data.name
     container_path = "/var/lib/postgresql/data"
   }
 }
@@ -42,22 +47,22 @@ resource "docker_container" "db_service" {
 resource "docker_container" "blog_service" {
   image = docker_image.blog_app.image_id
   name  = "blog_service"
-  
+
   networks_advanced {
-     name = docker_network.blog_network.name
+    name = docker_network.blog_network.name
   }
 
   ports {
-     internal = 8080
-     external = var.app_port
+    internal = 8080
+    external = var.app_port
   }
- 
+
   env = [
-      "POSTGRES_HOST=db_service",
-      "POSTGRES_USER=${var.postgres_user}",
-      "POSTGRES_PASSWORD=${var.postgres_password}",
-      "POSTGRES_PORT=${var.postgres_port}",
-      "POSTGRES_DB=${var.postgres_db}"
+    "POSTGRES_HOST=db_service",
+    "POSTGRES_USER=${var.postgres_user}",
+    "POSTGRES_PASSWORD=${var.postgres_password}",
+    "POSTGRES_PORT=${var.postgres_port}",
+    "POSTGRES_DB=${var.postgres_db}"
   ]
 
   # start after db container
--- terraform/docker/variables.tf
+++ terraform/docker/variables.tf
@@ -1,30 +1,30 @@
 variable "postgres_db" {
-    description = "DB for Blog App"
-    type = string
-    default = "blogs"
+  description = "DB for Blog App"
+  type        = string
+  default     = "blogs"
 }
 
 variable "postgres_user" {
-    description = "DB User for Blog App"
-    type = string
-    default = "postgres"
+  description = "DB User for Blog App"
+  type        = string
+  default     = "postgres"
 }
 
 variable "postgres_password" {
-    description = "DB Password for Blog App"
-    type = string
-    default = "admin"
-    sensitive = true
+  description = "DB Password for Blog App"
+  type        = string
+  default     = "admin"
+  sensitive   = true
 }
 
 variable "postgres_port" {
-    description = "DB Port"
-    type = number
-    default = 5432
+  description = "DB Port"
+  type        = number
+  default     = 5432
 }
 
 variable "app_port" {
-    description = "Blog App Port"
-    type = number
-    default = 8080
+  description = "Blog App Port"
+  type        = number
+  default     = 8080
 }
