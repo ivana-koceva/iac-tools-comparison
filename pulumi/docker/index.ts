b6460262b5986e200f891f0c76c5cdc9e8094f4f
Ivana Koceva <ivana.koceva10@gmail.com>
2025-11-02 21:10:04 +0100

    added config vars and secrets to test stack

--- pulumi/docker/index.ts
+++ pulumi/docker/index.ts
@@ -1,6 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
 
+// stack specific config vars
+const config = new pulumi.Config("blogs");
+const POSTGRES_DB = config.require("POSTGRES_DB");
+const POSTGRES_HOST = config.require("POSTGRES_HOST");
+const POSTGRES_PORT = config.requireNumber("POSTGRES_PORT");
+const APPLICATION_PORT = config.requireNumber("APPLICATION_PORT");
+const POSTGRES_USER = config.requireSecret("POSTGRES_USER");
+const POSTGRES_PASSWORD = config.requireSecret("POSTGRES_PASSWORD");
+
+const stack = pulumi.getStack();
+
 // create images
 const blog_app = new docker.RemoteImage("blog_app", {name: "ivanakoceva/blog-app:latest"});
 const blog_db = new docker.RemoteImage("blog_db", {name: "postgres:17-alpine"});
@@ -19,13 +30,13 @@ const db_service = new docker.Container("db_service", {
         name: blog_network.name,
     }],
     ports: [{
-        internal: 5432,
-        external: 5432,
+        internal: POSTGRES_PORT,
+        external: POSTGRES_PORT,
     }],
     envs: [
-        "POSTGRES_DB=blogs",
-        "POSTGRES_USER=postgres",
-        "POSTGRES_PASSWORD=admin"
+        `POSTGRES_DB=${POSTGRES_DB}`,
+        pulumi.interpolate`POSTGRES_USER=${POSTGRES_USER}`,
+        pulumi.interpolate`POSTGRES_PASSWORD=${POSTGRES_PASSWORD}`
     ],
     volumes: [{
         containerPath: "/var/lib/postgresql/data",
@@ -40,18 +51,18 @@ const blog_service = new docker.Container("blog_service", {
         name: blog_network.name,
     }],
     ports: [{
-        internal: 8080,
-        external: 8080,
+        internal: APPLICATION_PORT,
+        external: APPLICATION_PORT,
     }],
     envs: [
-        "POSTGRES_HOST=db_service",
-        "POSTGRES_DB=blogs",
-        "POSTGRES_USER=postgres",
-        "POSTGRES_PASSWORD=admin",
-        "POSTGRES_PORT=5432",
+        `POSTGRES_HOST=${POSTGRES_HOST}`,
+        `POSTGRES_DB=${POSTGRES_DB}`,
+        pulumi.interpolate`POSTGRES_USER=${POSTGRES_USER}`,
+        pulumi.interpolate`POSTGRES_PASSWORD=${POSTGRES_PASSWORD}`,
+        `POSTGRES_PORT=${POSTGRES_PORT}`,
     ],
     volumes: [{
         containerPath: "/var/lib/postgresql/data",
         volumeName: blogdb_data.name,
     }],
-}, {dependsOn: db_service});
+}, {dependsOn: [db_service]});
